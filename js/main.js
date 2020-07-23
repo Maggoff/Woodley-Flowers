33a4df526e21b72665fa47be064b65ba99744405
Maggoff <magmbov@gmail.com>
2020-07-23 15:46:12 +0300

    Fix bugs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+  $("#menu").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
+  });
+});
+
+
+$(document).ready(function(){
   $('.portfolio__slider').slick({
     centerMode: true,
     centerPadding: '0px',
