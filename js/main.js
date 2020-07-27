27ca4a747778a3fb4c6ffee15fef67685c9b1419
Maggoff <magmbov@gmail.com>
2020-07-27 13:26:46 +0300

    Add price range

--- js/main.js
+++ js/main.js
@@ -38,3 +38,24 @@ $(document).ready(function(){
 $(document).on('click', '.checkbox__item .checkbox__item__label', function (e) {
   e.stopPropagation();
 });
+
+let slider = document.getElementById('range');
+
+noUiSlider.create(slider, {
+    start: [550, 1290],
+    connect: true,
+    step: 1,
+    range: {
+        'min': 500,
+        'max': 3000
+    }
+});
+
+let valuesDivs = [
+  document.getElementById('range__min'),
+  document.getElementById('range__max')
+];
+
+range.noUiSlider.on('update', function (values, handle) {
+  valuesDivs[handle].value = Math.trunc( values[handle] );
+});
